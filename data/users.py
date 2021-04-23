221115a46d030167f702d76729759d2cdcbcdab3
elizabethuglova <elizabethuglova@yandex.ru>
2021-04-23 15:09:48 +0300

    конечный вариант с базой данных 3

--- data/users.py
+++ data/users.py
@@ -1,8 +1,10 @@
 import sqlalchemy
+from flask_login import UserMixin
+
 from .db_session import SqlAlchemyBase
 
 
-class Users(SqlAlchemyBase):
+class Users(SqlAlchemyBase, UserMixin):
     __tablename__ = 'users'
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
     name = sqlalchemy.Column(sqlalchemy.String(50), nullable=False)
